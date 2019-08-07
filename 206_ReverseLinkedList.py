72a201fb8c457047a9461717f226a08f3a48837d
Xixi Zhao <helloocc@users.noreply.github.com>
2019-08-07 09:11:58 +0800

    Create 206_ReverseLinkedList.py

---
+++ 206_ReverseLinkedList.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Solution:
+    def reverseList1(self, head: ListNode) -> ListNode:
+        """
+        非递归：
+        从头结点开始反转，头结点的next变成None，最后一次while循环反转尾结点，之后current变成current.next为None，所以返回pre结点。
+        """
+        pre = None
+        current = head
+        while current:
+            tmp = current.next
+            current.next = pre
+            pre = current
+            current = tmp
+        return pre
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        """
+        递归：
+        递归到尾结的前一个结点，每次反转当前结点的next结点，next结点指向head，head指向None。
+        """
+        if (not head) or (not head.next):
+            return head
+
+        current = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return current
+
+
+def print_node(head: ListNode):
+    _str = ''
+    while head:
+        _str += str(head)
+        head = head.next
+    print(_str)
+
+
+if __name__ == "__main__":
+    a, b, c, d = ListNode(1), ListNode(2), ListNode(3), ListNode(4)
+    a.next, b.next, c.next = b, c, d
+    print_node(a)
+    s = Solution()
+    print_node(s.reverseList(a))
