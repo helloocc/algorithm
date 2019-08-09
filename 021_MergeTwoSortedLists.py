494128fd1839c7a9151046685f330fbb15eacaf5
Xixi Zhao <helloocc@users.noreply.github.com>
2019-08-09 15:59:53 +0800

    Create 021_MergeTwoSortedLists.py

---
+++ 021_MergeTwoSortedLists.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Solution:
+    def mergeTwoLists1(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        非递归：生成哨兵结点。每次比较l1和l2的大小，新head每次指向值比较小的结点。
+        """
+        dummy_node = current = ListNode(None)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next = l1 or l2
+        return dummy_node.next
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        递归：递归比较下一个结点和另一方结点。
+        """
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+
+def print_node(head: ListNode):
+    _str = ''
+    while head:
+        _str += str(head)
+        head = head.next
+    print(_str)
+
+
+if __name__ == "__main__":
+    a, b, c = ListNode(1), ListNode(2), ListNode(4)
+    a.next, b.next, = b, c
+    d, e, f = ListNode(1), ListNode(3), ListNode(4)
+    d.next, e.next, = e, f
+    s = Solution()
+    print_node(s.mergeTwoLists(a, d))
+
+    a = ListNode(1)
+    d = ListNode(2)
+    s = Solution()
+    print_node(s.mergeTwoLists(a, d))
