7ad2bb3f1db3dbb729229c43144180b5ec5fa4c3
Xixi Zhao <helloocc@users.noreply.github.com>
2019-08-12 16:40:30 +0800

    Create 876_MiddleoftheLinkedList.py

---
+++ 876_MiddleoftheLinkedList.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __str__(self):
+        return str(self.val)
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        """
+        快慢指针，当快指针走到尾时，慢指针走到中间。
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+
+def print_node(head: ListNode):
+    _str = ''
+    while head:
+        _str += str(head)
+        head = head.next
+    print(_str)
+
+
+if __name__ == "__main__":
+    a, b, c, d = ListNode(1), ListNode(2), ListNode(3), ListNode(4)
+    a.next, b.next, c.next = b, c, d
+    print_node(a)
+    s = Solution()
+    print_node(s.middleNode(a))
