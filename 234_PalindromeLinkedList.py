120139925cdaedbc78702eeb5e75b81c3e3add19
Xixi Zhao <4636271+helloocc@users.noreply.github.com>
2019-09-06 16:28:02 +0800

    Create 234_PalindromeLinkedList.py

---
+++ 234_PalindromeLinkedList.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding=utf8 -*-
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return True
+        stack = []
+        slow = fast = head
+        stack.append(slow.val)
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            stack.append(slow.val)
+            if not fast.next:
+                stack.pop()
+        while slow.next:
+            if slow.next.val != stack.pop():
+                return False
+            slow = slow.next
+        return True
+
+
+def test1():
+    s = Solution()
+    a, b, c, d = ListNode(1), ListNode(2), ListNode(2), ListNode(1)
+    a.next, b.next, c.next = b, c, d
+    assert s.isPalindrome(a) == True
+
+    a, b, c, d, e = ListNode(1), ListNode(
+        2), ListNode(3), ListNode(2), ListNode(1)
+    a.next, b.next, c.next, d.next = b, c, d, e
+    assert s.isPalindrome(a) == True
+
+
+def test2():
+    s = Solution()
+    e, f = ListNode(1), ListNode(2)
+    e.next = f
+    assert s.isPalindrome(e) == False
+
+    e, f = ListNode(1), ListNode(1)
+    e.next = f
+    assert s.isPalindrome(e) == True
+
+    e = ListNode(1)
+    assert s.isPalindrome(e) == True
