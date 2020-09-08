e5f4b63fb94d12338256f722a58300111439a317
Xixi Zhao <4636271+helloocc@users.noreply.github.com>
2020-09-08 17:05:01 +0800

    Update 347_TopKFrequentElements.py

--- 347_TopKFrequentElements.py
+++ 347_TopKFrequentElements.py
@@ -6,15 +6,22 @@ from typing import List
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent1(self, nums: List[int], k: int) -> List[int]:
         from collections import Counter
         count = Counter(nums)
         return [x[0] for x in count.most_common(k)]
 
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        """
+        TODO: 堆排序
+        """
+        pass
 
-@pytest.mark.parametrize(('nums', 'k', 'res'), [([1, 1, 1, 2, 2, 3], 2, [1, 2]),
-                                                ([1], 1, [1]),
-                                                ([1, 2, 2], 1, [2])])
+
+@pytest.mark.parametrize(('nums', 'k', 'res'), [
+    ([1, 1, 1, 2, 2, 3], 2, [1, 2]),
+    ([1], 1, [1]),
+    ([1, 2, 2], 1, [2])])
 def test1(nums, k, res):
     s = Solution()
-    assert s.topKFrequent(nums, k) == res
+    assert s.topKFrequent1(nums, k) == res
